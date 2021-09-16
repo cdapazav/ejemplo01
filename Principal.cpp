e754a95504d3e7980d0665b25296a40ff005977c
PLANET PC <PLANET PC@KOKUN-PC>
2021-09-16 02:43:56 -0400

    Agregar archivos de proyecto.

---
+++ Principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Nombre: Apaza Villca Cristofer Denilson" << std::endl;
+	std::cout << "Carrera: Ingenieria de sistemas" << std::endl;
+	std::cout << "CI: 10349416" << std::endl;
+	std::cout << "CU: 35-4436" << std::endl;
+	std::cout << "telefono: 71176124" << std::endl;
+
+	return 0;
+}
